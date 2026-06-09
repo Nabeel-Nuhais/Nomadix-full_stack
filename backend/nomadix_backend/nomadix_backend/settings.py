4f5551cc5b2d5ac73c0bf90e3fc7889f2e818b9d
nabeel nuhais <nabeelnuhais@gmail.com>
2026-06-09 15:57:49 +0530

    backend hosting completed

--- backend/nomadix_backend/nomadix_backend/settings.py
+++ backend/nomadix_backend/nomadix_backend/settings.py
@@ -120,6 +120,9 @@ DATABASES = {
         'PASSWORD': os.environ.get("SQL_PASSWORD"),
         'HOST': os.environ.get("SQL_HOST"),
         'PORT': os.environ.get("SQL_PORT"),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
